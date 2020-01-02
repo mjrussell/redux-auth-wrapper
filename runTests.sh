a73d55cb1c23062a204a8d3109f57e756c63f623
Matthew Russell <matt@simspace.com>
2020-01-01 22:09:57 -0800

    Remove --

--- runTests.sh
+++ runTests.sh
@@ -6,10 +6,10 @@ rm -rf node_modules/history
 if [ "$REACT_ROUTER_VERSION" = "3" ]; then
   yarn add react-router@3.0.2
   yarn add react-router-redux@4.0.8
-  yarn run test:cov -- test/rrv3-test.js
+  yarn run test:cov test/rrv3-test.js
 elif [ "$REACT_ROUTER_VERSION" = "4" ]; then
   yarn add react-router@4.0.0
   yarn add history@4.6.1
   yarn add react-router-redux@5.0.0-alpha.6
-  yarn run test:cov -- test/rrv4-test.js
+  yarn run test:cov test/rrv4-test.js
 fi
